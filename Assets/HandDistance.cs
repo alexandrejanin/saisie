79e4124f6f256ef838a7f8669cdeadf7d5dd7d0d
Alexandre Janin <alexandrejanin92@gmail.com>
2019-12-13 09:40:30 +0100

    Premier prototype

---
+++ Assets/HandDistance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap.Unity;
+
+public class HandDistance : MonoBehaviour {
+[SerializeField] private Transform leftHand, rightHand;
+
+[SerializeField] private float maxDistance, minDistance;
+
+[SerializeField] private Image image;
+
+    void Update() {
+        if (!leftHand.gameObject.active || !rightHand.gameObject.active){
+            Debug.LogWarning("Il manque une main!");
+            image.color = Color.black;
+            return;
+        }
+
+        var distance = (leftHand.position - rightHand.position).magnitude;
+
+        var t = Mathf.InverseLerp(minDistance, maxDistance, distance);
+
+        var color =  Color.Lerp(Color.white, Color.black, t);
+
+        image.color = color;
+    }
+}
---
+++ Assets/HandText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandText : MonoBehaviour {
+    [SerializeField] private Transform leftHand, rightHand;
+    private Text text;
+
+    void Awake() {
+        text = GetComponent<Text>();
+    }
+
+    void Update(){
+     text.text = "";
+     text.text += $"Position main gauche: {leftHand.position}\n";
+     text.text += $"Position main droite: {rightHand.position}\n";
+     text.text += $"Rotation main gauche: {leftHand.rotation}\n";
+     text.text += $"Rotation main droite: {rightHand.rotation}\n";   
+    }
+}
