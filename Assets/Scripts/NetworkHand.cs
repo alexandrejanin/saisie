085d3481e57afe9f1131619fca90cea5dee62332
alexandrejanin <alexandrejanin92@gmail.com>
2019-12-18 00:06:38 +0100

    couleur écran selon position mains en réseua

--- Assets/Scripts/NetworkHand.cs
+++ Assets/Scripts/NetworkHand.cs
@@ -1,47 +1,68 @@
 ﻿using BeardedManStudios.Forge.Networking.Generated;
-using BeardedManStudios.Forge.Networking.Unity;
-using Leap.Unity;
 using UnityEngine;
 
-public class NetworkHand : HandBehavior
-{
-    [SerializeField] private Transform index;
+public class NetworkHand : HandBehavior {
+    [SerializeField] private Color errorColor, offColor, onColor;
+    [SerializeField] private float minDistance = 0.05f;
+    [SerializeField] private float maxDistance = 0.1f;
 
     private HandPositionManager handPositionManager;
+    private ScreenColorManager screenColorManager;
 
-    private void Awake()
-    {
+    private void Awake() {
         handPositionManager = FindObjectOfType<HandPositionManager>();
+        screenColorManager = FindObjectOfType<ScreenColorManager>();
     }
 
-    protected override void NetworkStart()
-    {
+    protected override void NetworkStart() {
         base.NetworkStart();
         networkObject.UpdateInterval = 20;
-        GetComponentInChildren<HandIndex>().enabled = networkObject.IsOwner;
-
-        foreach (var hmm in FindObjectsOfType<HandModelManager>())
-        {
-            hmm.gameObject.SetActive(NetworkManager.Instance.IsServer);
-        }
-        foreach (var lsp in FindObjectsOfType<LeapServiceProvider>())
-        {
-            lsp.gameObject.SetActive(NetworkManager.Instance.IsServer);
-        }
     }
 
-    private void Update()
-    {
+    private void Update() {
         if (!networkObject.NetworkReady)
             return;
 
-        if (networkObject.IsOwner)
-        {
-            networkObject.position = index.position;
+        if (!networkObject.IsOwner) {
+            transform.position = networkObject.position;
+            return;
         }
-        else
-        {
-            index.position = networkObject.position;
+
+        var index = handPositionManager.Index;
+
+        if (!index) {
+            screenColorManager.Color = errorColor;
+            return;
         }
+
+        transform.position = index.position;
+        networkObject.position = index.position;
+
+        var shortestDistance = Mathf.Infinity;
+
+        foreach (var decoy in FindObjectsOfType<StaticDecoy>()) {
+            var dist = Vector3.Distance(index.position, decoy.transform.position);
+            if (dist < shortestDistance)
+                shortestDistance = dist;
+        }
+
+        foreach (var decoy in FindObjectsOfType<Decoy>()) {
+            var dist = Vector3.Distance(index.position, decoy.transform.position);
+            if (dist < shortestDistance)
+                shortestDistance = dist;
+        }
+
+        foreach (var hand in FindObjectsOfType<NetworkHand>()) {
+            if (hand == this)
+                continue;
+            var dist = Vector3.Distance(index.position, hand.transform.position);
+            if (dist < shortestDistance)
+                shortestDistance = dist;
+        }
+
+        Debug.Log(shortestDistance);
+
+        var t = Mathf.InverseLerp(minDistance, maxDistance, shortestDistance);
+        screenColorManager.Color = Color.Lerp(onColor, offColor, t);
     }
 }
---
+++ Assets/Scripts/ScreenColorManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenColorManager : MonoBehaviour {
+    private Image image;
+
+    public Color Color {
+        set => image.color = value;
+    }
+
+    private void Awake() {
+        image = GetComponent<Image>();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.D))
+            image.enabled = !image.enabled;
+    }
+}
